f2590675ff4ad51c16cf6e22e00837570ce8c233
valenti-casas-ilerna <vcasas@ilerna.com>
2024-06-03 20:15:40 +0200

    Finished 'for' control flow

--- 2-ControlFlowLesson/src/app/app.component.ts
+++ 2-ControlFlowLesson/src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = '2-ControlFlowLesson';
+  
+  isUserLogged = true;
+  userBookmarks = [{id:0, url:'https://youtube.com'}, {id:1, url:'https://gmail.com'}, {id:2, url:'https://google.com'}];
+  myMap = new Map([["firstName", "Angular"],["lastName", "Framework"]]);
+
 }
