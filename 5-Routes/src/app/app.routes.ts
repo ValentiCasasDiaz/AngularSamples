829facda97b413f1fb17cc90b07a23d96a6b8b5a
valenti-casas-ilerna <vcasas@ilerna.com>
2024-06-10 12:52:50 +0200

    Finished routes sample

--- 5-Routes/src/app/app.routes.ts
+++ 5-Routes/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',                       // Nombre de la ruta para acceder
+        title: 'App Home Page',         // Título del contenido actual (aparece en la pestaña del navegador)
+        component: HomeComponent,       // Componente que se mostrará
+    },
+    {
+        path: 'user',                       
+        title: 'User Detail Page',         
+        component: UserComponent,
+    },
+];
