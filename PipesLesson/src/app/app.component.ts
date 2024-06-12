5732ed361494d730d79e752ab8f0d0bdf8eb6eb4
valenti-casas-ilerna <vcasas@ilerna.com>
2024-06-12 13:41:22 +0200

    Finished examples

--- PipesLesson/src/app/app.component.ts
+++ PipesLesson/src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UpperCasePipe, LowerCasePipe, JsonPipe, DecimalPipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'PipesLesson';
+  user = { id: 0, name: 'Pere', age: 18, img: 'https://picsum.photos/200' };
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+  
 }
