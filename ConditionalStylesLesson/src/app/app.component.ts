82b0a94bfbf931b2c810a67f44e5aa5671bd3849
Valenti Casas <vcasas@ilerna.com>
2024-10-29 11:13:39 +0100

    Finished ngClass & ngStyle

--- ConditionalStylesLesson/src/app/app.component.ts
+++ ConditionalStylesLesson/src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ConditionalStylesLesson';
+  checked: boolean = false;
+
+  getButtonClass() {
+    return {
+      btn: true,
+      'btn-primary': this.checked == true,
+      'btn-secondary': this.checked == false,
+    };
+  }
+
+  calcularEstilo() {
+    return {
+      background: 'yellow',
+      width: '300px'
+    };
+  }
 }
